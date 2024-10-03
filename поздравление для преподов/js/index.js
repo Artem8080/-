e43dc1f3baab4254ee63e2a9130a90e2628b3d4d
Artem8080 <anadoan47@icloud.com>
2024-10-03 17:04:56 +0300

    Add files via upload

---
+++ поздравление для преподов/js/index.js
@@ -0,0 +1,135 @@
+// объявление переменных
+
+const Nikita = document.querySelector('#Nikita__button')
+const Nikita__front = document.querySelector('#Nikita__front')
+const Nikita__back = document.querySelector('#Nikita__back')
+
+const Elena = document.querySelector('#Elena__button')
+const Elena__front = document.querySelector('#Elena__front')
+const Elena__back = document.querySelector('#Elena__back')
+
+const Marina = document.querySelector('#Marina__button')
+const Marina__front = document.querySelector('#Marina__front')
+const Marina__back = document.querySelector('#Marina__back')
+
+const Daria = document.querySelector('#Daria__button')
+const Daria__front = document.querySelector('#Daria__front')
+const Daria__back = document.querySelector('#Daria__back')
+
+const Oresto = document.querySelector('#Oresto__button')
+const Oresto__front = document.querySelector('#Oresto__front')
+const Oresto__back = document.querySelector('#Oresto__back')
+
+const Milana = document.querySelector('#Milana__button')
+const Milana__front = document.querySelector('#Milana__front')
+const Milana__back = document.querySelector('#Milana__back')
+
+const IrinaIvanovna = document.querySelector('#IrinaIvanovna__button')
+const IrinaIvanovna__front = document.querySelector('#IrinaIvanovna__front')
+const IrinaIvanovna__back = document.querySelector('#IrinaIvanovna__back')
+
+const Ludmila = document.querySelector('#Ludmila__button')
+const Ludmila__front = document.querySelector('#Ludmila__front')
+const Ludmila__back = document.querySelector('#Ludmila__back')
+
+const Tamara = document.querySelector('#Tamara__button')
+const Tamara__front = document.querySelector('#Tamara__front')
+const Tamara__back = document.querySelector('#Tamara__back')
+
+const Lili = document.querySelector('#Lili__button')
+const Lili__front = document.querySelector('#Lili__front')
+const Lili__back = document.querySelector('#Lili__back')
+
+const Zaira = document.querySelector('#Zaira__button')
+const Zaira__front = document.querySelector('#Zaira__front')
+const Zaira__back = document.querySelector('#Zaira__back')
+
+const Kostik = document.querySelector('#Kostik__button')
+const Kostik__front = document.querySelector('#Kostik__front')
+const Kostik__back = document.querySelector('#Kostik__back')
+
+const Rita = document.querySelector('#Rita__button')
+const Rita__front = document.querySelector('#Rita__front')
+const Rita__back = document.querySelector('#Rita__back')
+
+const Irina_Dmitrievna = document.querySelector('#Irina_Dmitrievna__button')
+const Irina_Dmitrievna__front = document.querySelector('#Irina_Dmitrievna__front')
+const Irina_Dmitrievna__back = document.querySelector('#Irina_Dmitrievna__back')
+
+
+// объявление переменных
+
+// изменения при клике
+
+Elena.addEventListener('click', () => {
+    Elena__front.classList.toggle('Elena-frontOn')
+    Elena__back.classList.toggle('Elena-backOn')
+})
+
+Nikita.addEventListener('click', () => {
+    Nikita__front.classList.toggle('Nikita-frontOn')
+    Nikita__back.classList.toggle('Nikita-backOn')
+})
+
+
+Marina.addEventListener('click', () => {
+    Marina__front.classList.toggle('Marina-frontOn')
+    Marina__back.classList.toggle('Marina-backOn')
+})
+
+Daria.addEventListener('click', () => {
+    Daria__front.classList.toggle('Daria-frontOn')
+    Daria__back.classList.toggle('Daria-backOn')
+})
+
+Oresto.addEventListener('click', () => {
+    Oresto__front.classList.toggle('Oresto-frontOn')
+    Oresto__back.classList.toggle('Oresto-backOn')
+})
+
+Milana.addEventListener('click', () => {
+    Milana__front.classList.toggle('Milana-frontOn')
+    Milana__back.classList.toggle('Milana-backOn')
+})
+
+IrinaIvanovna.addEventListener('click', () => {
+    IrinaIvanovna__front.classList.toggle('IrinaIvanovna-frontOn')
+    IrinaIvanovna__back.classList.toggle('IrinaIvanovna-backOn')
+})
+
+Ludmila.addEventListener('click', () => {
+    Ludmila__front.classList.toggle('Ludmila-frontOn')
+    Ludmila__back.classList.toggle('Ludmila-backOn')
+})
+
+Tamara.addEventListener('click', () => {
+    Tamara__front.classList.toggle('Tamara-frontOn')
+    Tamara__back.classList.toggle('Tamara-backOn')
+})
+
+Lili.addEventListener('click', () => {
+    Lili__front.classList.toggle('Lili-frontOn')
+    Lili__back.classList.toggle('Lili-backOn')
+})
+
+Zaira.addEventListener('click', () => {
+    Zaira__front.classList.toggle('Zaira-frontOn')
+    Zaira__back.classList.toggle('Zaira-backOn')
+})
+
+Kostik.addEventListener('click', () => {
+    Kostik__front.classList.toggle('Kostik-frontOn')
+    Kostik__back.classList.toggle('Kostik-backOn')
+})
+
+Rita.addEventListener('click', () => {
+    Rita__front.classList.toggle('Rita-frontOn')
+    Rita__back.classList.toggle('Rita-backOn')
+})
+
+Irina_Dmitrievna.addEventListener('click', () => {
+    Irina_Dmitrievna__front.classList.toggle('Irina_Dmitrievna-frontOn')
+    Irina_Dmitrievna__back.classList.toggle('Irina_Dmitrievna-backOn')
+})
+
+// изменения при клике
